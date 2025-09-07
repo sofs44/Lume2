144e23e34ab5ce55a881f7463286391ec39307fc
sofs44 <sofia.sc.080207@gmail.com>
2025-09-07 19:09:37 -0300

    Add files via upload

--- config/urls.py
+++ config/urls.py
@@ -1,39 +1,22 @@
-# config/urls.py
-
-from django.contrib import admin
 from django.urls import path
-from django.contrib.auth import logout
-from django.shortcuts import redirect
-from app import views
-
-
-
-from app.views import (
-    AuthView, IndexView, DiarioView, criar_diario, DeleteDiarioView,
-    EditDiarioView, CheckinView, ConteudoView,
-    listar_notificacoes, minhas_favoritas,
-    metas_checkin_redirect, metas_usuario, metas_psicologo
-)
+from .views import AuthView, IndexView, ConteudoView, CheckinView, DiarioView, criar_diario, DeleteDiarioView, EditDiarioView, listar_notificacoes, minhas_favoritas, metas_checkin_redirect, metas_psicologo, metas_usuario
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', IndexView.as_view(), name='index'),
+    path('auth/', AuthView.as_view(), name='auth'),
+    path('', include('app.urls')),
+    path('conteudo/', ConteudoView.as_view(), name='conteudo'),
+    path('checkin/', CheckinView.as_view(), name='checkin'),
 
-    # ROTA DE LOGIN/CADASTRO unificada:
-    path('', AuthView.as_view(), name='auth'),
-    path('logout/', lambda r: (logout(r), redirect('auth'))[1], name='logout'),
-
-    # Demais rotas da sua aplicação:
-    path('index/', IndexView.as_view(), name='index'),
     path('diario/', DiarioView.as_view(), name='diario'),
     path('diario/criar/', criar_diario, name='criar_diario'),
-    path('delete/<int:id>/', DeleteDiarioView.as_view(), name='delete'),
-    path('edit/<int:id>/', EditDiarioView.as_view(), name='edit_diario'),
-    path('checkin/', CheckinView.as_view(), name='checkin'),
-    path('conteudo/', ConteudoView.as_view(), name='conteudo'),
-    path('notificacoes/', listar_notificacoes, name='listar_notificacoes'),
-    path('favoritas/', minhas_favoritas, name='minhas_favoritas'),
-    path('metas/', metas_checkin_redirect, name='metas'),
-    path('metas/usuario/', metas_usuario, name='metas_usuario'),
-    path('metas/psicologo/', metas_psicologo, name='metas_psicologo'),
+    path('diario/<int:id>/delete/', DeleteDiarioView.as_view(), name='delete_diario'),
+    path('diario/<int:id>/edit/', EditDiarioView.as_view(), name='edit_diario'),
 
+    path('notificacoes/', listar_notificacoes, name='notificacoes'),
+    path('favoritas/', minhas_favoritas, name='favoritas'),
+
+    path('metas/', metas_checkin_redirect, name='metas'),
+    path('metas/psicologo/', metas_psicologo, name='metas_psicologo'),
+    path('metas/usuario/', metas_usuario, name='metas_usuario'),
 ]
